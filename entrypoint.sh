cea4b76c3140c40af187052629aff2fb529014c6
Arkaitz Jimenez <arkaitzj@gmail.com>
2022-08-06 10:29:50 +0200

    add destination if target_ip supplied

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,16 @@ function main() {
 
     set -x 
 
+    if [ -n "${TARGET_IP}" ]; then
+        DESTINATION="-d ${TARGET_IP}"
+    fi
+
     for ports in $@; do
         echo "$ports"
         from=$(echo "$ports" | cut -d ':' -f 1)
         to=$(echo "$ports" | cut -d ':' -f 2)
 
-        iptables -t nat -A PREROUTING -p tcp -i eth0 --dport $from -j REDIRECT --to-port $to
+        iptables -t nat -A PREROUTING -p tcp -i eth0 ${DESTINATION} --dport $from -j REDIRECT --to-port $to
     done
 
     iptables -t nat --list
