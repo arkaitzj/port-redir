dd29b3a74c0fb0008c9c6c35df700b14eb67dd5c
Arkaitz Jimenez <arkaitzj@gmail.com>
2023-01-02 14:18:30 +0100

    Add rule for localhost output too

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,11 @@ function main() {
         from=$(echo "$ports" | cut -d ':' -f 1)
         to=$(echo "$ports" | cut -d ':' -f 2)
 
+        # For outside connections
         iptables -t nat -A PREROUTING -p tcp -i eth0 ${DESTINATION} --dport $from -j REDIRECT --to-port $to
+
+        # For localhost connections, like istio sidecars
+        iptables -t nat -A OUTPUT -p tcp -o lo -d ${DESTINATION} --dport $from -j REDIRECT --to-port $to
     done
 
     iptables -t nat --list
